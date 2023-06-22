ad54f8843c4b747352593df4176ce5e0a6dce4f3
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-22 14:45:58 +0200

    feat: add class Constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export default class Constants {
+  static WELCOME = 'Welcome to the File Manager,';
+  static GOODBYE_MESS = 'Thank you for using File Manager,';
+  static GOODBYE = ', goodbye!';
+  static END_OF_INPUT = 'You are currently in ';
+  static INVALID_INPUT = 'Invalid input';
+  static ERROR_MESS = 'Operation failed'; 
+}
