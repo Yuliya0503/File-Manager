1c6762a53c021b8b0b8f900cb3e3fa25e7cbde08
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-23 16:46:09 +0200

    feat: add show current directory

--- src/utils/statuses.js
+++ src/utils/statuses.js
@@ -1,4 +1,5 @@
 import Messages from "../constants/messages.js";
+import Currentdirectory from "./currentDirectory.js";
 
 export const showOperationFailed = async() => {
   try {
@@ -12,6 +13,16 @@ export const showOperationFailed = async() => {
 export const showInvalidInput = async() => {
   try {
     console.log(`\x1b[33m${Messages.INVALID_INPUT}\x1b[0m`);
+  }
+  catch(error) {
+    throw new Error(error);
+  }
+}
+
+export const showCurrentDir = async() => {
+  try { 
+  const currDir = new Currentdirectory();
+  console.log(`\x1b[32m${Messages.END_OF_INPUT + ' ' + currDir.getCyrrentDir()}\x1b[0m`)
   }
   catch(error) {
     throw new Error(error);
