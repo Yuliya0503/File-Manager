aed2822682d8ba061e1c8a6e10369689792b9332
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-23 15:14:26 +0200

    feat: add hunlder error to hello and bye scripts

--- src/utils/helloAndBye.js
+++ src/utils/helloAndBye.js
@@ -2,14 +2,24 @@ import Messages from "../constants/messages.js";
 import { getUserName } from "./username.js";
 
 const sayHiToUser = async() => {
-  const username = await getUserName();
-  const welcome = `${Messages.WELCOME}, ${username}!`;
-  console.log(`\x1b[35m${welcome}\x1b[0m`);
+  try{
+    const username = await getUserName();
+    const welcome = `${Messages.WELCOME}, ${username}!`;
+    console.log(`\x1b[35m${welcome}\x1b[0m`);
+  }
+  catch(error){
+    throw new Error(error);
+  }
 };
 
-const sayByeToUser = async() =>{
-  const username = await getUserName();
-  const goodbye = `${Messages.GOODBYE_MESS}, ${username}, ${Messages.GOODBYE}!`
-  console.log(`\x1b[35m${goodbye}\x1b[0m`);
+const sayByeToUser = async() => {
+  try{
+    const username = await getUserName();
+    const goodbye = `${Messages.GOODBYE_MESS}, ${username}, ${Messages.GOODBYE}!`
+    console.log(`\x1b[35m${goodbye}\x1b[0m`);
+  }
+  catch(error){
+    throw new Error(error);
+  }
 }
 export { sayHiToUser, sayByeToUser };
