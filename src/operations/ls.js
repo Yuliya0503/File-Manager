f933a2e8bd86f018d91bf8101ac317b957c2001c
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-25 17:51:50 +0200

    feat: add ls script

---
+++ src/operations/ls.js
@@ -0,0 +1,26 @@
+import Currentdirectory from '../utils/currentDirectory.js';
+import fs from 'fs/promises';
+
+
+export  const ls = async() => {
+  try {
+    const currentdirectory = new Currentdirectory();
+    const currDir = currentdirectory.getCyrrentDir();
+    const dirList = await fs.readdir(currDir, {withFileTypes: true});
+    const filesArr = dirList
+      .filter(file => file.isDirectory() || file.isFile())
+      .map((file) => {
+        const type = file.isDirectory() ? 'directory' : 'file';
+        return {
+          'name': file.name, 
+          'type': type
+        };
+      })
+      .sort((a, b) => a.type === b.type ? a.name.localeCompare(b.name) : a.type === 'file' ? 1 : -1) 
+    console.table(filesArr);
+  }
+  catch(error){
+    throw new Error(error);
+  }
+};
+
