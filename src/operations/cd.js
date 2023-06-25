7347c98ecba6fcc31e627eb64e8d479361e25257
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-25 21:42:46 +0200

    feat: add error handler to cd script

--- src/operations/cd.js
+++ src/operations/cd.js
@@ -2,10 +2,13 @@ import { getAbsolutePath, checkExistPath } from "../utils/helpers.js";
 import { currentDirectory } from "../utils/currentDirectory.js";
 
 export const cd = async(command) => {
-  const pathNew = command.arguments;
-  const newAbsolutePath = await getAbsolutePath(pathNew);
-
-  await checkExistPath(newAbsolutePath);
-
-  currentDirectory.setCyrrentDir(newAbsolutePath); 
+  try{
+    const pathNew = command.arguments;
+    const newAbsolutePath = await getAbsolutePath(pathNew);
+    await checkExistPath(newAbsolutePath);
+    currentDirectory.setCyrrentDir(newAbsolutePath); 
+  }
+  catch(error){
+    throw new Error(error);
+  }
 }
