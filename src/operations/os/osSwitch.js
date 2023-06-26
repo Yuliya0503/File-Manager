a3c323ddb168d184e3b652f0a2353516a42d5fc2
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-26 14:48:43 +0200

    feat: add username script

--- src/operations/os/osSwitch.js
+++ src/operations/os/osSwitch.js
@@ -2,6 +2,7 @@ import Commands from '../../constants/commands.js';
 import { eol } from './eol.js';
 import { cpus } from './cpus.js';
 import { showHomedir } from './homedir.js';
+import { userName } from './username.js';
 
 export const os = async(command) => {
   const commandArg = command.arguments[0];
@@ -14,6 +15,9 @@ export const os = async(command) => {
       break;
     case Commands.OS_HOMEDIR:
       await showHomedir();
+      break;
+    case Commands.OS_USERNAME:
+      await userName();
       break;
   } 
 }
