f17f1a52795241db87d8dc2ade760388d493e7f3
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-06-26 13:40:24 +0200

    feat: add os messages constants

---
+++ src/constants/osMessages.js
@@ -0,0 +1,9 @@
+export default class Messages {
+  static END_OF_LINE_MESS = 'Default system End-Of-Line';
+  static HOME_DIRECOTRY_MESS = 'Home directory';
+  static SYSTEM_USER_NAME = 'System user name';
+  static CPUS_ARCHITECTURE = 'CPU architecture';
+  static CPUS_MODEL = 'CPU model';
+  static CPUS_CLOCK_RATE = 'CPU clock rate, GHz';
+  static CPUS_AMOUNT = 'Amount of CPUS';
+}
